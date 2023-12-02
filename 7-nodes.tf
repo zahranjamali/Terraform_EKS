56a6a15891967950a981096533b6e31441613d7e
zahranjamali <zahranjamali@gmail.com>
2023-12-02 07:40:38 +0530

    added public subnet in node-group

--- 7-nodes.tf
+++ 7-nodes.tf
@@ -35,7 +35,9 @@ resource "aws_eks_node_group" "private-nodes-1" {
 
   subnet_ids = [
     aws_subnet.private-us-east-1a.id,
-    aws_subnet.private-us-east-1b.id
+    aws_subnet.private-us-east-1b.id,
+    aws_subnet.public-us-east-1a.id,
+    aws_subnet.public-us-east-1b.id
   ]
 
   capacity_type  = "ON_DEMAND"
