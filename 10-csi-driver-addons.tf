6ef44787b43306268d031910191e0f5a3cacaad4
zahranjamali <zahranjamali@gmail.com>
2023-12-02 19:27:47 +0530

    working commit

--- 10-csi-driver-addons.tf
+++ 10-csi-driver-addons.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_addon" "csi_driver" {
   cluster_name             = aws_eks_cluster.demo1.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.23.0-eksbuild.1" #you have to manually update version by knowing current version by below command
+  addon_version            = "v1.25.0-eksbuild.1" #you have to manually update version by knowing current version by below command
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
 }
 
--- 7-nodes.tf
+++ 7-nodes.tf
@@ -34,8 +34,6 @@ resource "aws_eks_node_group" "private-nodes-1" {
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
-    aws_subnet.private-us-east-1a.id,
-    aws_subnet.private-us-east-1b.id,
     aws_subnet.public-us-east-1a.id,
     aws_subnet.public-us-east-1b.id
   ]
@@ -44,7 +42,7 @@ resource "aws_eks_node_group" "private-nodes-1" {
   instance_types = ["t3.large"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 2
     min_size     = 0
   }
